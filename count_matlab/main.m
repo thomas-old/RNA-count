0bc0cfee5cf26d29614fcaeeb602ae5860197995
Thomas Hu <thomashu.fr@gmail.com>
2020-05-19 13:56:07 -0400

    changed reading file and some fixes

--- count_matlab/main.m
+++ count_matlab/main.m
@@ -4,10 +4,6 @@ clear all
 %% Global parameters
 
 addpath('functions')
-
-tune_segmentation = false;
-tune_spot_counting = false;
-
 %% Read image from a directory
 [file,path] = uigetfile('*.tif');
 if isequal(file,0)
@@ -17,7 +13,7 @@ else
 end
 
 % Read the image
-img = imread(append(path,file));
+img = imread(strcat(path,file));
 img_gray = rgb2gray(img);
 img_adjusted = imadjust(img_gray);
 
@@ -25,6 +21,8 @@ img_adjusted = imadjust(img_gray);
 img_filtered = imgaussfilt(img_adjusted,4);
 
 %% Thresholding level selection
+tune_segmentation = false;
+
 if tune_segmentation ~= 0
     plot_seg_threshold(img);
 end
@@ -33,10 +31,12 @@ end
 threshold = 50;
 [~,BW] = mask(threshold, img_filtered, 20, 10);
 plot_cc(BW, img);
-
-%% Remove Small Objects defined by the radius
 CC = bwconncomp(BW);
 stats = regionprops(BW,'all');
+
+%% Remove Small Objects defined by the radius
+close all
+
 radius = 800;
 L = labelmatrix(CC);
 
@@ -45,6 +45,8 @@ BW = ismember(L, find([stats.Area] >= radius));
 [L,~] = plot_cc(BW, img);
 
 %% Manually remove Connected Components (CC)
+close all
+
 c_list = [2 13];
 BW2 = remove_cc(BW, L, c_list);
 
@@ -58,8 +60,6 @@ L(L== 10) = 3;
 
 %% Plot new region segmmentation
 
-stats = regionprops(L,'all');
-
 plot_seg(L, img);
 Lrgb = label2rgb(L,'jet','k','shuffle');
 
@@ -72,6 +72,8 @@ Lrgb = label2rgb(L,'jet','k','shuffle');
 
 %% Find threshold for counting the number of RNA dot
 
+close all
+
 [file,path] = uigetfile('*.tif');
 if isequal(file,0)
    disp('User selected Cancel');
@@ -82,12 +84,15 @@ end
 tune_spot_countintg = true;
 
 % Read the image
-img = imread(append(path,file));
+img = imread(strcat(path,file));
 % Laplacian of Guassian filter on the image
 img_d = double(img);
 img_filtered = LOG_filter(img_d);
 % Normalize img2
 img_filtered = img_filtered/max(img_filtered(:));
+
+%% Plot histogram of the image for threshold intensity selection
+tune_spot_counting = false;
 
 if tune_spot_counting ~= 0
 plot_spot_threshold(img_filtered)
